a64d8c5f60acbad677311aacb09a1ba779bf9629
123Xylem <cucullen111@gmail.com>
2024-09-19 17:10:34 +0100

    cart models + user app relation

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from product.models import Product
+from user.models import User
 
 # Create your models here.
+class Cart(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  product_list = models.ManyToManyField(Product)
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from product.models import *
 
-# Create your views here.
+
+def add_to_cart(request, id):
+  #create and/or add product id to cart
+  product = get_object_or_404(Product, id=id)
+
+  print(id, product)
+  pass
+  return product
+
+def remove_from_cart(request, id):
+  #Remove product id from cart
+  pass
+
+def checkout(request, id):
+  if id:
+    #Add product to cart and checkout
+    pass
+  else:
+    # checkout cart for payment
+    pass
+
