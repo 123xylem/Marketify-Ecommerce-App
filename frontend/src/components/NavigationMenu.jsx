98c29321b1c317c6d51038a87f125b51cc628b9d
123Xylem <cucullen111@gmail.com>
2024-10-01 13:46:00 +0100

    Basic nav with submenu

--- frontend/src/components/NavigationMenu.jsx
+++ frontend/src/components/NavigationMenu.jsx
@@ -1,15 +1,31 @@
 import { Outlet, Link } from "react-router-dom";
 import {
   AppBar,
-  Button,
+  ListItemIcon,
   IconButton,
   Stack,
   Toolbar,
   Typography,
+  Menu,
+  MenuItem,
 } from "@mui/material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
 import StorefrontIcon from "@mui/icons-material/Storefront";
 
+import { useState } from "react";
+
 const NavigationMenu = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const open = Boolean(anchorEl);
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -19,26 +35,55 @@ const NavigationMenu = () => {
           to="/"
           edge="start"
           aria-label="logo"
+          style={{ color: "white", marginRight: "auto" }}
         >
           <StorefrontIcon />
+          <Typography
+            variant="h6"
+            component="div"
+            style={{ paddingLeft: ".5rem" }}
+          >
+            Marketify
+          </Typography>
         </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Marketify
-        </Typography>
         <Stack direction="row" spacing={2}>
-          <Link to="/">Home</Link>
-          <Link to="/api/accountprofile/account">Profile</Link>
-          <Link to="/contact">Contact</Link>
+          <MenuItem component={Link} to="/">
+            Home
+          </MenuItem>
+          <MenuItem
+            component={Link}
+            to="#"
+            onClick={handleClick}
+            endicon={<KeyboardArrowDownIcon />}
+          >
+            Products
+            <ListItemIcon
+              style={{ minWidth: 0, marginLeft: "auto", color: "white" }}
+            >
+              <ArrowDropDownIcon />
+            </ListItemIcon>
+          </MenuItem>
+          <MenuItem component={Link} to="/api/accountprofile/account">
+            Profile
+          </MenuItem>
+          <MenuItem component={Link} to="/contact">
+            Contact
+          </MenuItem>
+          <Menu
+            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+            transformOrigin={{ vertical: "top", horizontal: "center" }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+          >
+            <MenuItem to="/products/category/tech" component={Link}>
+              Tech
+            </MenuItem>
+            <MenuItem to="/products/category/sport" component={Link}>
+              Sports
+            </MenuItem>
+          </Menu>
         </Stack>
-        {/* <ul>
-          <li></li>
-          <li>
-            <Link to="/api/accountprofile/account">Profile</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul> */}
       </Toolbar>
       <Outlet />
     </AppBar>
