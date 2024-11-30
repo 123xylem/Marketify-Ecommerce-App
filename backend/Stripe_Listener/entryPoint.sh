8230a346c1f4bff7aa82f318139bd8d6617b8007
Chris Cullen <cucullen111@gmail.com>
2024-11-30 13:33:34 +0000

    Update entryPoint.sh

--- backend/Stripe_Listener/entryPoint.sh
+++ backend/Stripe_Listener/entryPoint.sh
@@ -1,9 +1,6 @@
 
 # #!/bin/bash
-# stripe listen \
-#   --api-key "$STRIPE_SK" \
-#   stripe listen --events payment_intent.created,customer.created,payment_intent.succeeded,charge.succeeded,checkout.session.completed,charge.failed  
-#   --forward-to "$BACKEND_DOMAIN"/api/orders/stripe/payment-handler/
+
 
 #!/bin/bash
 
@@ -11,6 +8,6 @@ echo "STRIPE_SK: $STRIPE_SK"
 echo "BACKEND_DOMAIN: $BACKEND_DOMAIN"
 
 # Start listening to Stripe events and forward to the backend
-stripe listen \
+stripe listen --events payment_intent.created,customer.created,payment_intent.succeeded,charge.succeeded,checkout.session.completed,charge.failed \
   --api-key "$STRIPE_SK" \
   --forward-to "$BACKEND_DOMAIN/api/orders/stripe/payment-handler/"
