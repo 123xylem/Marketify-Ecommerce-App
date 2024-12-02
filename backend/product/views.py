9616589e2d6185736156232d99ab957eaa03c9e9
123Xylem <cucullen111@gmail.com>
2024-12-02 13:35:14 +0000

    replace runserver with gunicorn. add log middleware for req timings

--- backend/product/views.py
+++ backend/product/views.py
@@ -11,7 +11,6 @@ from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.decorators import permission_classes
-
 class HomePageView(ListView):
     model = Product
     template_name = 'home.html'
@@ -24,13 +23,13 @@ class ProductViewSet(viewsets.ModelViewSet):
     lookup_fields = ['pk', 'slug']
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+
     def get_queryset(self):
         """
         Use URL Get to determine queryset
         """
         queryset = self.queryset
         category = self.request.query_params.get('cat', None)
-        print(self.request.query_params, 'list')
         if category:
             #link title of category to a product that has one of these via the ID's
             cat = Category.objects.filter(title__iexact=category).first()
@@ -42,21 +41,18 @@ class ProductViewSet(viewsets.ModelViewSet):
             queryset = Product.objects.filter(Q(title__icontains=search) | Q(description__icontains=search))
             return queryset
         
-        return  Product.objects.all()
+        return Product.objects.all()
 
 
     def list(self, request, *args, **kwargs) -> NoReturn:
         serializer = self.get_serializer(self.get_queryset(), many=True)
-        # print(serializer.data)
         return Response(serializer.data)
     
     @action(detail=False, methods=['post'])
     def search(self,request):
-        print(request, 'POST')
         return Response(request)
 
     def retrieve(self, request, *args, **kwargs):
-        print('retrieve')
         lookup_value = self.kwargs.get('pk') 
         if lookup_value.isdigit():  
             product = get_object_or_404(self.queryset, pk=lookup_value)
@@ -79,10 +75,9 @@ class ProductViewSet(viewsets.ModelViewSet):
 @permission_classes([AllowAny])
 @extend_schema(responses=CategorySerializer)
 class CategoryView(APIView):
-    permission_classes = [AllowAny]  # This should allow unauthenticated access
+    permission_classes = [AllowAny] 
 
     def get(self, request):
         queryset = Category.objects.all()
         serializer = CategorySerializer(queryset, many=True).data
         return Response(serializer)
-# return Response({"categories": "Category data"})
