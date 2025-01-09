9e5078da6894585e79afaee0b295f8d0f5b78231
123Xylem <cucullen111@gmail.com>
2025-01-09 21:55:57 +0000

    change var name

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
 export const ACCESS_TOKEN = "access-token";
 export const REFRESH_TOKEN = "refresh-token";
-export const BACKEND_DOMAIN = import.meta.env.VITE_BACKEND_URL;
+export const BACKEND_DOMAIN = import.meta.env.VITE_BACKEND_DOMAIN;
 export const VITE_OAUTH_CLIENT_ID = import.meta.env.VITE_OAUTH_CLIENT_ID;
 export const VITE_OAUTH_CLIENT_SECRET = import.meta.env
   .VITE_OAUTH_CLIENT_SECRET;
